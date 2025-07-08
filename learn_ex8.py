eaa4d2ec61f238d01bb9be86f02957f62275b646
37023538yutomiura <hmm86480@kwansei.ac.jp>
2025-07-08 16:22:24 +0900

    ex8提出

--- learn_ex8.py
+++ learn_ex8.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     model = YOLO("yolov8m.pt")
-    results = model.train(data="dataset.yaml", epochs=50)
+    results = model.train(data="dataset_ex8.yaml", epochs=150)
 
